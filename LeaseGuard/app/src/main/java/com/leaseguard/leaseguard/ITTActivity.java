09e2e2b2d22240b4aa83a744d8ffec620f74dd76
MSI\ssos3 <ssos3182@gmail.com>
2020-06-16 19:17:16 -0400

    Fixed bugs

--- LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/ITTActivity.java
+++ LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/ITTActivity.java
@@ -1,14 +1,19 @@
 package com.leaseguard.leaseguard;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Collections;
 
 /**
  * Created by Sang on 6/11/2020.
@@ -46,16 +51,37 @@ public class ITTActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_CODE && resultCode == RESULT_OK) {
-            bitmap.recycle();
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
             bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
         }
     }
 
     private void startCameraFlow() {
-        if (checkCameraHardware(this)) {
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, CAMERA_CODE);
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            if (checkCameraHardware(this)) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_CODE);
+            }
+        } else {
+            String[] requests = {Manifest.permission.CAMERA};
+            requestPermissions(requests, CAMERA_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_CODE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startCameraFlow();
+                }
         }
     }
 
