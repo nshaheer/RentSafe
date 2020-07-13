63403d87ab073f4a85380c7d5d49ed7833776283
Sang <ssos3182@hotmail.com>
2020-07-13 03:34:30 -0400

    Added MenuButton and ActionMenu UI components.

--- LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/landing/SafeRentActivity.kt
+++ LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/landing/SafeRentActivity.kt
@@ -1,6 +1,7 @@
 package com.leaseguard.leaseguard.landing
 
 import android.os.Bundle
+import android.view.View
 import com.leaseguard.leaseguard.BaseActivity
 import com.leaseguard.leaseguard.R
 import kotlinx.android.synthetic.main.activity_saferent.*
@@ -14,9 +15,22 @@ class SafeRentActivity : BaseActivity<SafeRentActivityViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saferent)
-        pdfButton.setOnClickListener {
-
-        }
+        floatingActionMenu.addItem("Google Drive", R.drawable.ic_folder,
+                View.OnClickListener {
+                    // TODO: open drive
+                })
+        floatingActionMenu.addItem("PDF", R.drawable.ic_pdf,
+                View.OnClickListener {
+                    // TODO: open pdf
+                })
+        floatingActionMenu.addItem("Library", R.drawable.ic_lib,
+                View.OnClickListener {
+                    // TODO: open library
+                })
+        floatingActionMenu.addItem("Photo", R.drawable.ic_camera,
+                View.OnClickListener {
+                    // TODO: open camera
+                })
     }
 
     override fun getViewModel(): SafeRentActivityViewModel {
