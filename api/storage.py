65bc62f66d38433735955ffa685614185cf549f5
Salman Tariq <stariqmi@gmail.com>
2020-07-16 16:55:38 -0700

    STM - Setup mongo connection and storage

--- api/storage.py
+++ api/storage.py
@@ -1,8 +1,12 @@
 from abc import ABCMeta, abstractmethod
 
+from uuid import uuid4
+from pymongo import MongoClient
+
 
 class StorageInterface(metaclass=ABCMeta):
-    def add_lease(self, id, paragraphs, **kwargs):
+    @abstractmethod
+    def add_lease(self, paragraphs, **kwargs):
         pass
 
 
@@ -10,10 +14,27 @@ class MemStorage(StorageInterface):
     def __init__(self):
         self.storage = {}
 
-    def add_lease(self, id, paragraphs, **kwargs):
+    def add_lease(self, paragraphs, **kwargs):
         lease = {
             "paragraphs": paragraphs,
         }
         lease.update(kwargs)
 
-        self.storage[id] = lease
+        self.storage[str(uuid4())] = lease
+
+
+class MongoStorage(StorageInterface):
+    def __init__(self):
+        # Connection setup
+        conn = MongoClient(
+            "mongodb+srv://admin:jLtC_Q4wHJmKZ4BfvaCR@rent-safe-test-db.ldmk8.mongodb.net/<dbname>?retryWrites=true&w=majority"
+        )
+        self.storage = conn["rent-safe"]["leases"]
+
+    def add_lease(self, paragraphs, **kwargs):
+        lease = {
+            "paragraphs": paragraphs,
+        }
+        lease.update(kwargs)
+
+        return self.storage.insert_one(lease).inserted_id
