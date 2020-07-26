81682da704b82f7e868a41a82603e825c1f2f9b0
Salman Tariq <stariqmi@gmail.com>
2020-07-26 16:46:59 -0700

    STM - AWS Comprehend Entity Recognizer skeleton

--- api/infrastructure/entity_recognizer.py
+++ api/infrastructure/entity_recognizer.py
@@ -11,3 +11,23 @@ class DummyEntityRecog(EntityRecogInterface):
         # Upload to S3 - CSV in valid format
         # Call AWS Boto3 Comprehend Job
         return 1
+
+
+class AWSComprehendEntityRecognizer(EntityRecogInterface):
+    @staticmethod
+    def _compile_csv_string(paragraphs):
+        pass
+
+    @staticmethod
+    def _upload_csv_to_s3(csv_string):
+        pass
+
+    @staticmethod
+    def _schedule_recognition_job(s3_path):
+        pass
+
+    def recognize(self, paragraphs):
+        csv_string = self._compile_csv_string(paragraphs)
+        s3_path = self._upload_csv_to_s3(csv_string)
+
+        return self._schedule_recognition_job(s3_path)
