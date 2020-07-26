d7c3dabb11b3efac7eb3c32022b39f58f91fce87
Salman Tariq <stariqmi@gmail.com>
2020-07-26 13:32:40 -0700

    STM - Structure of Classification job

--- api/infrastructure/classifier.py
+++ api/infrastructure/classifier.py
@@ -11,3 +11,23 @@ class DummyClassifier(ClassifierInterface):
         # Upload to S3 - CSV in valid format
         # Call AWS Boto3 Custom Comprehend Job
         return 1
+
+
+class AwsComprehendClassifier(ClassifierInterface):
+    @staticmethod
+    def _compile_csv_string(paragraphs):
+        pass
+
+    @staticmethod
+    def _upload_csv_to_s3(csv_string):
+        pass
+
+    @staticmethod
+    def _schedule_classification_job(s3_path):
+        pass
+
+    def classify(self, paragraphs):
+        csv_string = self._compile_csv_string(paragraphs)
+        s3_path = self._upload_csv_to_s3(csv_string)
+
+        return self._schedule_classification_job(s3_path)
