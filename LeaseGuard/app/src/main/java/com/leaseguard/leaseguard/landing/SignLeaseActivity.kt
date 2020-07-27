01acf786ba37912e12aef8f644332b66640cf575
adhanji <aleem@chalk.com>
2020-07-27 17:31:00 -0400

    fix: add support for back button on SignLeaseActivity

--- LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/landing/SignLeaseActivity.kt
+++ LeaseGuard/app/src/main/java/com/leaseguard/leaseguard/landing/SignLeaseActivity.kt
@@ -1,9 +1,13 @@
 package com.leaseguard.leaseguard.landing
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.leaseguard.leaseguard.R
 import com.simplify.ink.InkView
+import dagger.android.support.DaggerAppCompatActivity
 
 class SignLeaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +21,16 @@ class SignLeaseActivity : AppCompatActivity() {
         ink.setColor(resources.getColor(android.R.color.black))
         ink.setMinStrokeWidth(1.5f)
         ink.setMaxStrokeWidth(6f)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                setResult(DaggerAppCompatActivity.RESULT_OK)
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
