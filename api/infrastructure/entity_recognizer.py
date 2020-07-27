128431bc99717e8e63f99f5ce14f043b5e7131ca
Salman Tariq <stariqmi@gmail.com>
2020-07-26 17:33:46 -0700

    STM - Adding files to S3 as part of recognition

--- api/infrastructure/entity_recognizer.py
+++ api/infrastructure/entity_recognizer.py
@@ -1,4 +1,7 @@
+from uuid import uuid4
 from abc import ABCMeta, abstractmethod
+
+import boto3
 
 
 class EntityRecogInterface(metaclass=ABCMeta):
@@ -14,20 +17,34 @@ class DummyEntityRecog(EntityRecogInterface):
 
 
 class AWSComprehendEntityRecognizer(EntityRecogInterface):
-    @staticmethod
-    def _compile_csv_string(paragraphs):
-        pass
+    def __init__(self, bucket):
+        self.bucket = bucket
 
     @staticmethod
-    def _upload_csv_to_s3(csv_string):
-        pass
+    def _compile_txt_string(paragraphs):
+        return "\n".join(paragraphs)
 
-    @staticmethod
-    def _schedule_recognition_job(s3_path):
-        pass
+    def _upload_txt_to_s3(self, txt_string):
+        s3_key = "lease-data/input/{}.txt".format(str(uuid4()))
+
+        session = boto3.Session(
+            aws_access_key_id="AKIAQCL63YTSKPN5MAFU",
+            aws_secret_access_key="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",
+            region_name="ca-central-1",
+        )
+
+        s3 = session.resource("s3")
+        s3_object = s3.Object(self.bucket, s3_key)
+        s3_object.put(Body=txt_string.encode("utf-8"))
+
+        return s3_key
+
+    def _schedule_recognition_job(self, s3_key):
+        s3_path = self.bucket + "/" + s3_key
+        return s3_path
 
     def recognize(self, paragraphs):
-        csv_string = self._compile_csv_string(paragraphs)
-        s3_path = self._upload_csv_to_s3(csv_string)
+        txt_string = self._compile_txt_string(paragraphs)
+        s3_key = self._upload_txt_to_s3(txt_string)
 
-        return self._schedule_recognition_job(s3_path)
+        return self._schedule_recognition_job(s3_key)
