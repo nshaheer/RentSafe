b9023b489c70fb1cf1bf1179498f25d6a126b8d3
Salman Tariq <stariqmi@gmail.com>
2020-08-01 20:43:27 -0700

    STM - Update image extraction

--- api/samples/extraction.py
+++
@@ -1,8 +0,0 @@
-from infrastructure.extractor import GoogleVisionExtractor
-
-def main():
-   MyGV = GoogleVisionExtractor()
-   MyGV.extract(1, "/Users/Shaheer/Desktop/25_RENTAL_APPLICATION_group_2018_12.pdf")
-   print("Hello")
-   
-   
