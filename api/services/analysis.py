1d08d28662292df9a57100970984572f0c8e08ec
Salman Tariq <stariqmi@gmail.com>
2020-08-04 01:56:02 -0700

    STM - Capture Security, Rent and Key deposit classifications

--- api/services/analysis.py
+++ api/services/analysis.py
@@ -1,11 +1,17 @@
 class AnalysisService:
     PETS_PROHIBITED_CLASS = "Pets Prohibited"
     GUESTS_PROHIBITED_CLASS = "Guests Prohibited"
+    SECURITY_DEPOSIT_REQUIRED = "Security Deposit"
+    RENT_DEPOSIT_REQUIRED = "Rent Deposit"
+    KEY_DEPOSIT_REQUIRED = "Key Deposit"
 
     @staticmethod
     def analyze_classification_results(results):
         pets_prohibited = False
         guests_prohibited = False
+        security_deposit_required = False
+        rent_deposit_required = False
+        key_deposit_required = False
 
         confident_results = (r for r in results if r["Classes"][0]["Score"] > 0.85)
 
@@ -16,10 +22,19 @@ class AnalysisService:
                 pets_prohibited = True
             elif classification == AnalysisService.GUESTS_PROHIBITED_CLASS:
                 guests_prohibited = True
+            elif classification == AnalysisService.SECURITY_DEPOSIT_REQUIRED:
+                security_deposit_required = True
+            elif classification == AnalysisService.RENT_DEPOSIT_REQUIRED:
+                rent_deposit_required = True
+            elif classification == AnalysisService.KEY_DEPOSIT_REQUIRED:
+                key_deposit_required = True
 
         return {
             "ArePetsProhibited": pets_prohibited,
             "AreGuestsProhibited": guests_prohibited,
+            "IsSecurityDepositRequired": security_deposit_required,
+            "IsRentDepositRequired": rent_deposit_required,
+            "IsKeyDepositRequired": key_deposit_required,
         }
 
     @staticmethod
