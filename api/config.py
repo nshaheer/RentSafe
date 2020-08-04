9d578ad7efc169193adffe4fecf151256a677c67
Salman Tariq <stariqmi@gmail.com>
2020-08-04 04:21:27 -0700

    STM - Config refactor

--- api/config.py
+++ api/config.py
@@ -2,3 +2,6 @@ class Config:
     CELERY_BROKER_URL = "redis://redis:6379"
     CELERY_RESULT_BACKEND = "redis://redis:6379"
     LEASE_DOCS_FOLDER = "/tmp/lease-docs"
+    AWS_S3_BUCKET = "rent-safe"
+    AWS_COMPREHEND_ROLE_ARN = "arn:aws:iam::005097899236:role/RentSafeAWSComprehendRole"
+    AWS_COMPREHEND_CLASSIFIER_ARN = "arn:aws:comprehend:ca-central-1:005097899236:document-classifier/RentalClassifier-v1-0-0"
